22451069410b222211ce79fd5c18d7b03ba26e3c
Avanti-shitole <avantishitole974@gmail.com>
2026-09-11 21:14:33 +0530

    Add user registration

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 
-urlpatterns =[
-     path('', views.student_list, name='student_list'),
+urlpatterns = [
+    path('', views.student_list, name='student_list'),
 
+    # URL for user registration
+    path('register/', views.register, name='register'),
+    # URL for user login
+    path('login/', views.login_view, name='login'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 
 def student_list(request):
     return render(request, 'students/student_list.html')
+
+
+
+#  user registration
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        messages.success(request, 'Registration successful! Please login.')
+        return redirect('login')
+
+    return render(request, 'students/register.html')
+
+# user login
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('student_list')
+
+    return render(request, 'students/login.html')
